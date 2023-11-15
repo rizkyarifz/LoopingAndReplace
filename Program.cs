cc365614030ab20e6921e6edbbfe879e3c7b4310
Rizky Arif Zaelani <rizkyarif.z24@gmail.com>
2023-11-15 12:52:03 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿bool inputAgain = true;
+while (inputAgain)
+{
+    Console.Write("Enter the value of N: ");
+    int nValue = int.Parse(Console.ReadLine());
+    for (int i = 1; i <= nValue; i++)
+    {
+        if (i % 5 == 0 && i != 5)
+        {
+            Console.Write("AERO ");
+        }
+        else if (i % 6 == 0 && i != 6)
+        {
+            Console.Write("ASYST ");
+        }
+        else
+        {
+            Console.Write($"{i} ");
+        }
+    }
+    Console.WriteLine();
+    Console.Write("Again? (y/n) ");
+    inputAgain = Console.ReadLine() == "y" ? true: false;
+}
